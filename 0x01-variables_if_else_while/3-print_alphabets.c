8231d66af7d1ec5c1326f490903a6fee6eaf519d
Esther2860 <essy2860@gmail.com>
2023-02-17 12:22:44 -0800

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main entry - int main func
+ * description - print alphabet in lowercase
+ * and in uppercase followed by a newline
+ * author: Esther Muthoni
+ */
+
+int main(void)
+{
+char ch;
+for(ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for(ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return(0);
+}
